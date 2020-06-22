41887fc7c985ca3eca0e84c8e9fb4ef061e23272
analubarreto <analuibm@gmail.com>
2020-06-22 13:08:19 -0300

    added more folder structure

---
+++ src/event/EventDetail/EventDetailPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Event detailed paged</h1>
+    </div>
+  )
+}
---
+++ src/features/user/Settings/SettingsNav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SettingsNav = () => {
+  return (
+    <div>
+      <h1>Nav Settings</h1>
+    </div>
+  )
+}
